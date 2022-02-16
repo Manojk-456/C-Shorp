76222520abe7f00ff9b09d1164bfded56140e11b
Manoj Kumar Yadav <78580176+Manojk-456@users.noreply.github.com>
2022-02-17 02:17:50 +0530

    Add files via upload
    This is a Factorial Program.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace Hello_Program
+namespace Factorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Program");
+            Console.WriteLine("Enter the number to find Factorial :");
+            int fact = 1;
+            int n = int.Parse(Console.ReadLine());
+            while(n>0)
+            {
+                fact = fact * n;
+                n--;
+            }
+            Console.WriteLine("factorial of this number is : " + fact);
+            Console.ReadLine();
         }
     }
 }

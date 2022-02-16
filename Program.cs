fffa60c884084d7baadfdd0b42a6d3677c6c0e7a
Manoj Kumar Yadav <78580176+Manojk-456@users.noreply.github.com>
2022-02-17 02:25:04 +0530

    Add files via upload
    Table Printing .

--- Program.cs
+++ Program.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 
-namespace SumOfNumber
+namespace Table
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the max number to find range sum :");
-            int n=int.Parse(Console.ReadLine());
-            int sum = 0;
-            while(n>0)
+            Console.WriteLine("Enter a number to find Table :");
+            int n = int.Parse(Console.ReadLine());
+            for(int i=1;i<=10;i++)
             {
-                sum += n;
-                Console.Write(n + "\t");
-                n--;
+                Console.WriteLine(n + " * " + i + " = " + n * i);
             }
-            Console.WriteLine();
-            Console.WriteLine("sum of the number is : " + sum);
+            Console.ReadLine();
         }
     }
 }

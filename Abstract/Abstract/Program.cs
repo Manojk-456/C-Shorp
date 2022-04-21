0139ad76a947f13dee7121b29d6cfe5b65851d35
admin <admin@CHETUIWK1622>
2022-04-21 19:42:09 +0530

    abstraction class and Exception handling

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+
+
+    abstract class abc:check
+    {
+        public abstract void test(int num1, int num2);
+        public void show()
+        {
+            Console.WriteLine("Hello manoj kumar :");
+        }
+
+    }
+
+    class add : abc
+    {
+        public override void test(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine("sum of {0} and {1} number is : {2}", num1, num2, sum);
+        }
+    }
+    class sub : abc
+    {
+        public override void test(int num1, int num2)
+        {
+            int sub = num1 - num2;
+            Console.WriteLine("sub  of {0} and {1} number is : {2}", num1, num2, sub);
+        }
+    }
+
+    class mult : abc
+    {
+        public override void test(int num1, int num2)
+        {
+            int mult = num1 * num2;
+            Console.WriteLine("mult of {0} and {1} number is : {2}", num1, num2, mult);
+        }
+    }
+
+    class div : abc
+    {
+        public override void test(int num1, int num2)
+        {
+            int div = num1 / num2;
+            Console.WriteLine("div  of {0} and {1} number is : {2}", num1, num2, div);
+        }
+    }
+
+    class mod : abc
+    {
+        public override void test(int num1, int num2)
+        {
+            int mod = num1 % num2;
+            Console.WriteLine("mod  of {0} and {1} number is : {2}", num1, num2, mod);
+        }
+    }
+
+    class check
+    {
+        public void check1()
+        {
+            Console.WriteLine("check method is inherited in the parent class : ");
+        }
+    }
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            add obj = new add();
+            abc pobj;
+            pobj = obj;
+            pobj.show();
+            pobj.check1();
+            obj.test(12, 23);
+
+            sub obj1 = new sub();
+            obj1.test(12, 23);
+
+
+            mult obj2 = new mult();
+            obj2.test(12, 23);
+
+
+            div obj3 = new div();
+            obj3.test(12, 23);
+
+
+            mod obj4 = new mod();
+            obj4.test(12, 23);
+
+            
+            Console.ReadLine();
+
+
+        }
+       
+    }
+
+}
---
+++ Exception_Handling/Exception_Handling/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        int result=0;
+
+        //Program()
+        //{
+        //    result = 0;
+        //}
+
+        public void div(int a, int b)
+        {
+            try
+            {
+                result = a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("Result: {0}", result);
+            }
+        }
+        static void Main(string[] args)
+            {
+            Program obj = new Program();
+            obj.div(12, 0);
+            Console.ReadLine();
+        }
+        
+    }
+}
+
+
+/* usern define Exception */
+//using System;
+
+namespace UserDefinedException
+{
+    class TestTemperature
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            try
+            {
+                temp.showTemp();
+            }
+            catch (TempIsZeroException e)
+            {
+                Console.WriteLine("TempIsZeroException: {0}", e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+public class TempIsZeroException : Exception
+{
+    public TempIsZeroException(string message) : base(message)
+    {
+    }
+}
+public class Temperature
+{
+    int temperature =0;
+
+    public void showTemp()
+    {
+
+        if (temperature == 0)
+        {
+            throw (new TempIsZeroException("Zero Temperature found"));
+        }
+        else
+        {
+            Console.WriteLine("Temperature: {0}", temperature);
+        }
+    }
+}
